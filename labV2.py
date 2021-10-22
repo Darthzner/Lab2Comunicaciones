c7530dd96e60a276c87a1a39f08e3c72cdddfcae
Joaquín Fernández <67400456+Joako-Sador@users.noreply.github.com>
2021-10-22 18:16:23 -0300

    gracias lester

--- labV2.py
+++ labV2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 radianes = []
 radio = []
 
-angulo = [0,22.5,45,67.5,90,112.5,135,157.5,180,202.5,225,247.5,270,292.5,315,337.5,0]
+angulo = [0,22.5,45,67.5,90,112.5,135,157.5,180,202.5,225,247.5,270,292.5,315,337.5,360]
 
 for an in angulo: 
     radianes.append( an * np.pi / 180 )
@@ -13,8 +13,7 @@ dbm = [-62,-61,-52.5,-50,-43,-40,-39,-38,-41.2,-45,-50.5,-66,-66,-65.5,-61,-54.5
 
 #formula de friis
 Pt = 0
-Gt = 12 #bdi
-Gr = 10.24 #db
+Gr = -1.75 #db
 Gt = [] #dbm
 L = 20 * np.log( 4 * np.pi * 2.57 / 0.125) #dbm
 
@@ -24,7 +23,7 @@ for Pr in dbm:
 
 #normalizar valores
 for b in Gt: 
-    radio.append((b - Gt[0],2))
+    radio.append(round(b - Gt[0],2))
 
 #plot
 ax = plt.figure().add_subplot()
@@ -43,6 +42,6 @@ plt.title("Diagrama de radiación")
 plt.xlabel("Radianes/Ganancia(dB)")
 plt.show()
 
-print(Gt)
+#print(Gt)
 for i in range(0,len(radio)):
     print("(" + str(round(radianes[i],2)) + "," + str(radio[i]) + ")\n")
